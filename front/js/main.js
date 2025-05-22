0fa98ca26a0df6a0397edcc43ac305ebdd58a89b
LibiaManoela <libiamanoelas.hentz@gmail.com>
2025-05-22 16:43:34 -0300

    organização da pasta js

--- front/js/main.js
+++ front/js/main.js
@@ -31,20 +31,3 @@ function cookies(){
     const header  = document.getElementById('header')
     header.removeChild(cookies)
 }
-
-//Mostrar saldo
-function saldo(){
-    const saldo    = document.getElementById('valor')
-    const checkbox = document.getElementById('toggleSaldo')
-
-    const valor  = 'R$ 1000,00';
-    const oculto = '*********';
-
-    checkbox.addEventListener('change', function() {
-    saldo.textContent = this.checked ? valor : oculto;
-    });
-}
-
-window.onload = function() {
-    saldo();
-};
---
+++ front/js/saldo.js
@@ -0,0 +1,16 @@
+//Mostrar saldo
+function saldo(){
+    const saldo    = document.getElementById('valor')
+    const checkbox = document.getElementById('toggleSaldo')
+
+    const valor  = 'R$ 1000,00';
+    const oculto = '*********';
+
+    checkbox.addEventListener('change', function() {
+    saldo.textContent = this.checked ? valor : oculto;
+    });
+}
+
+window.onload = function() {
+    saldo();
+};
